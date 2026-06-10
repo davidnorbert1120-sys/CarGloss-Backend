4cb7131f9c9190c6853466aaf67d59f855c7688a
davidnorbert1120-sys <david.norbert1120@gmail.com>
2026-06-10 17:38:17 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/hu/cargloss/cargloss_backend/CarglossBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.cargloss.cargloss_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarglossBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarglossBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/cargloss/cargloss_backend/CarglossBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.cargloss.cargloss_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarglossBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
